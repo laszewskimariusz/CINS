48113b6768857d50660f0263a44ace8474d8e585
Mariusz Laszewski <laszewskimariusz@gmail.com>
2017-11-10 02:38:31 +0000

    MYSQL - fix
    Szybki fix, wczoraj nie upnołem wersji na mysql i cały czas pracowałem na lite.

--- cinsWeb/cinsWeb/settings.py
+++ cinsWeb/cinsWeb/settings.py
@@ -77,13 +77,19 @@ WSGI_APPLICATION = 'cinsWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'cinsdb',
+        'USER': 'root',
+        'PASSWORD': 'shinigami',
+        'HOST': '192.168.0.12',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
