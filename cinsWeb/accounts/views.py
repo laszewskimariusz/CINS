e1cb075324239ad9b6a4c07cc78cc84a4a7506b4
Mariusz Laszewski <laszewskimariusz@gmail.com>
2017-11-10 01:41:02 +0000

    Kawa time
    jw

--- cinsWeb/accounts/views.py
+++ cinsWeb/accounts/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.shortcuts import render, HttpResponse
 
 def home(request):
-    return render(request, 'accounts/login.html')
+    return render(request, 'accounts/home.html')
