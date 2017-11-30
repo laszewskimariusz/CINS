f016ece5f76c4e1cf63ddbb536d8881649f66361
Krzysztof Królikowski <kkrolikowski@gmail.com>
2017-11-30 23:16:30 +0100

    simple loop

---
+++ agent/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unistd.h>
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    int n = 0;
+    while (1)
+        cout << "Loop #" << n++ << " PID: " << getpid() << endl;
+
+    return 0; 
+}
